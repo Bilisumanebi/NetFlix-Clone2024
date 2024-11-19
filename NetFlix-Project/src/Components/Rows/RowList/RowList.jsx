f5aadf67dc692324e09eebe93cc9c4b260e55425
Bilisumanebi <hbilisuma@yahoo.com>
2024-11-19 00:06:01 -0600

    Row and RowList added

---
+++ NetFlix-Project/src/Components/Rows/RowList/RowList.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Row from '../Row/Row'
+import requests from '../../../Utils/requests'
+
+const RowList = () => {
+      return (
+            <>
+                  <Row
+                        title="Netflix originals"
+                        fetchUrl={requests.fetchNetflixOriginals}
+                        isLargeRow={true}
+                  />
+                  <Row
+                        title="Trending Now"
+                        fetchUrl={requests.fetchTrending}
+                        isLargeRow={true}
+                  />
+                  <Row 
+                        title="Commedies"
+                        fetchUrl={requests.fetchComedyMovies}
+                        isLargeRow={true} 
+                  />
+                  <Row
+                        title="Horror Movies"
+                        fetchUrl={requests.fetchHorrorMovies}
+                        isLargeRow={true}
+                  />
+                  <Row
+                        title="Top rated"
+                        fetchUrl={requests.fetchTopRatedMovies}
+                        isLargeRow={true}
+                  />
+                  <Row 
+                        title="Romance Movies"
+                        fetchUrl={requests.fetchRomanceMovies}
+                        isLargeRow={true}
+                  />
+                  <Row 
+                        title="Documentaries"
+                        fetchUrl={requests.fetchDocumentaries}
+                        isLargeRow={true}
+                  />
+
+            </>
+      )
+}
+
+export default RowList
